7703a64b9cce4a7ac151361d5d38f7c2c000e35d
Christian Bolus <christianjbolus@gmail.com>
2021-07-22 14:54:58 -0400

    Add cors, jwt dependencies and seed db

--- Gemfile
+++ Gemfile
@@ -14,7 +14,9 @@ gem 'puma', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+# Use Json Web Token (JWT) for token based authentication
+gem 'jwt'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -23,7 +25,7 @@ gem 'puma', '~> 5.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -33,6 +35,7 @@ end
 group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'pry-rails'
   gem 'spring'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chat.destroy_all
+User.destroy_all
+
+@elon = User.create!(email: 'elon@email.com', username: 'elonmusk', password: 'falcon9', display_name: 'Elon Musk', profile_pic: 'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg')
+@christian = User.create!(email: 'cbolus@email.com', username: 'christianbolus', password: 'monkey', display_name: 'Christian Bolus', profile_pic: 'https://res.cloudinary.com/ditt6ekpx/image/upload/v1626978009/GA%20Project%204/Headshot_Reduced_Cropped_sja1vr.jpg')
+@naval = User.create!(email: 'naval@email.com', username: 'naval', password: 'naval1', display_name: 'Naval Ravikant', profile_pic: 'https://theceolibrary.com/media/people-photos/naval-ravikant-books.jpg')
+
+puts "#{User.count} users created"
+
+chats = [
+  {
+    content: 'Desire is just a contract that you make with yourself to be unhappy until you get what you want',
+    likes: 384,
+    reposts: 173,
+    user: @naval
+  },
+  {
+    content: 'There’s nothing wrong with waiting until the last minute.  The secret is knowing when the last minute is.',
+    likes: 12,
+    reposts: 6,
+    user: @christian
+  },
+  {
+    content: 'Full test duration firing of 3 Raptors on Super Heavy Booster!',
+    likes: 120,
+    reposts: 87,
+    user: @elon
+  },
+  {
+    content: 'I am the Doge father.',
+    likes: 243,
+    reposts: 45,
+    user: @elon
+  },
+  {
+    content: 'The past is entirely contained in your head.  It is nowhere else.  The present is all that exists and this where you exist.',
+    likes: 380,
+    reposts: 173,
+    user: @naval
+  },
+  {
+    content: "You can't determine the quality of a decision based on the outcome.",
+    likes: 380,
+    reposts: 173,
+    user: @christian
+  }
+]
+
+Chat.create!(chats)
+
+puts "#{Chat.count} chats created"
+
