088db7a1d31eccf8c63da1847e794db694fcde1a
Christian Bolus <christianjbolus@gmail.com>
2021-07-23 19:51:51 -0400

    Render all of the replies to a particular Chat

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,6 @@ import Button from './Button';
 import Chat from './Chat';
 import Engagement from './Engagement';
 import FormInput from './FormInput';
-import Reply from './Reply';
 import TextArea from './TextArea';
 
-export { Button, Chat, Engagement, FormInput, Reply, TextArea };
+export { Button, Chat, Engagement, FormInput, TextArea };
--- client/src/screens/ChatDetail.jsx
+++ client/src/screens/ChatDetail.jsx
@@ -1,50 +1,49 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getOneChat } from '../services/chats';
-import { getAllReplies } from '../services/replies'
-import { Engagement, Reply } from '../components';
-
+import { getAllReplies } from '../services/replies';
+import { Engagement, Chat } from '../components';
 
 export default function ChatDetail() {
   const [chat, setChat] = useState(null);
-  const [replies, setReplies] = useState(null)
+  const [replies, setReplies] = useState(null);
   const { id } = useParams();
-  
+
   useEffect(() => {
     const fetchChat = async () => {
       const chatData = await getOneChat(id);
       setChat(chatData);
     };
     const fetchReplies = async () => {
-      const replyData = await getAllReplies(id)
-      setReplies(replyData)
+      const replyData = await getAllReplies(id);
+      setReplies(replyData);
     }
-    fetchChat()
+    fetchChat();
     fetchReplies()
   }, []);
-  
+
   return (
     <>
-    <div className="chat-detail-container">
-      <div className="chat-detail-user">
-        <img src={chat?.user.profile_pic} />
-        <div className="chat-detail-user-indetifiers">
-          <p>{chat?.user.display_name}</p>
-          <p>{chat?.user.username}</p>
+      <div className="chat-detail-container">
+        <div className="chat-detail-user">
+          <img src={chat?.user.profile_pic} />
+          <div className="chat-detail-user-indetifiers">
+            <p>{chat?.user.display_name}</p>
+            <p>{chat?.user.username}</p>
+          </div>
         </div>
+        <p className="chat-detail-text">{chat?.content}</p>
+        <Engagement
+          replies={chat?.reply_count}
+          reposts={chat?.repost_count}
+          likes={chat?.like_count}
+        />
       </div>
-      <p className="chat-detail-text">{chat?.content}</p>
-      <Engagement
-        replies={chat?.reply_count}
-        reposts={chat?.repost_count}
-        likes={chat?.like_count}
-      />
-    </div>
-    <div className="replies-container">
-      {replies?.map(reply => {
-        <Reply reply={reply} />
-      })}
-    </div>
+      <div className="replies-container">
+        {replies?.map(reply => (
+          <Chat chat={reply} key={reply.id}/>
+        ))}
+      </div>
     </>
   );
 }
