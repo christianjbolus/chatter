edfc44f21ab8b119605476d7827312840a4435d9
Christian Bolus <christianjbolus@gmail.com>
2021-07-22 20:53:47 -0400

    Add api calls to reply endpoints

---
+++ client/src/services/replies.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import api from './apiConfig';
+
+export const postReply = async (chatId, replyData) => {
+  try {
+    const res = await api.post(`/chats/${chatId}/replies`, { reply: replyData });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const putReply = async (chatId, replyId, replyData) => {
+  try {
+    const res = await api.put(`/chats/${chatId}/replies/${replyId}`, { reply: replyData });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteReply = async (chatId, replyId) => {
+  try {
+    const res = await api.delete(`/chats/${chatId}/replies/${replyId}`)
+    return res
+  } catch (error) {
+    throw error
+  }
+}
