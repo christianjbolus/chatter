efe947e2a8b5a3ce4fe7ce7cb8e8b3bc0393ec1b
Christian Bolus <christianjbolus@gmail.com>
2021-07-23 15:11:08 -0400

    Reformat Chat component

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Engagement from './Engagement';
 
 export default function Chat({ chat }) {
+  const history = useHistory();
   return (
     <div className="chat-container">
       <div className="user-img">
-        <img src={chat.user.profile_pic} />
+        <img className="user-profile-pic" src={chat.user.profile_pic} />
       </div>
-      <div className="chat-card">
-        <div className="user-identifiers">
-          <p className="chat-display_name">{chat.user.display_name}</p>
-          <p className="chat-username">@{chat.user.username}</p>
+      <div className="chat-card" onClick={history.push(`/chats/${chat.id}`)}>
+        <div className="chat-content">
+          <div className="user-identifiers">
+            <p className="chat-display_name">{chat.user.display_name}</p>
+            <p className="chat-username">@{chat.user.username}</p>
+          </div>
+          <p className="chat-text">{chat.content}</p>
         </div>
-        <p className="chat-text">{chat.content}</p>
         <Engagement
           replies={chat.reply_count}
           reposts={chat.repost_count}
