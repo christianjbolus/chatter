e68aac9cc488d6401bc1c760a5aad881f76a14aa
Christian Bolus <christianjbolus@gmail.com>
2021-07-25 00:33:31 -0400

    Update meta-data on Chat when reply created

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,5 +1,6 @@
 class RepliesController < ApplicationController
   before_action :set_reply, only: %i[show update destroy]
+  before_action :set_chat, only: %i[create destroy]
   before_action :authorize_request, only: %i[create update destroy]
 
   def index
@@ -19,10 +20,12 @@ class RepliesController < ApplicationController
   # POST /replies
   def create
     @reply = Reply.new(reply_params)
-    @reply.chat = Chat.find(params[:chat_id])
+    @reply.chat = @chat
     @reply.user = @current_user
+    @chat.reply_count += 1 unless @chat.reply_count.nil?
+    @chat.reply_count = 1 if @chat.reply_count.nil?
 
-    if @reply.save
+    if @reply.save && @chat.save
       render json: @reply, status: :created
     else
       render json: @reply.errors, status: :unprocessable_entity
@@ -40,6 +43,9 @@ class RepliesController < ApplicationController
 
   # DELETE /replies/1
   def destroy
+    @chat.reply_count = nil if @chat.reply_count == 1
+    @chat.reply_count -= 1 unless @chat.reply_count.nil?
+    @chat.save
     @reply.destroy
   end
 
@@ -48,6 +54,10 @@ class RepliesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_reply
     @reply = Reply.find(params[:id])
+  end
+
+  def set_chat
+    @chat = Chat.find(params[:chat_id])
   end
 
   # Only allow a list of trusted parameters through.
