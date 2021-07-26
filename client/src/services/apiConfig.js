3e519751574209d85a772512dbe63a7d6f76a06a
Christian Bolus <christianjbolus@gmail.com>
2021-07-26 14:32:16 -0400

    Add _redirects file for front-end deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'deployed url'
+      ? 'https://chatter-app-server.herokuapp.com/'
       : 'http://localhost:3000',
 });
 
