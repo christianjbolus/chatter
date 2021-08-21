e2d2721dfdb24a833f10c6f24e5a9853e535eddd
Christian Bolus <christianjbolus@gmail.com>
2021-08-20 21:45:11 -0400

    Update styling of compose page

--- client/pages/chats/compose.js
+++ client/pages/chats/compose.js
@@ -1,7 +1,7 @@
 import { /*useContext,*/ useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Button, TextArea } from '../components';
+import { Button, TextArea } from '../../components';
 import { IoArrowBackOutline } from '@react-icons/all-files/io5/IoArrowBackOutline';
 import styles from '../../styles/Compose.module.css';
 import icons from '../../styles/Icon.module.css';
@@ -33,13 +33,13 @@ export default function ChatCreate({ handleCreate }) {
           />
         </div>
         <div className={styles.form_group}>
-          <Link href={`/users/${currentUser?.username}`}>
+          {/* <Link href={`/users/${currentUser?.username}`}> */}
             <img
               className={styles.profile_pic}
-              src={currentUser?.profile_pic}
-              alt={currentUser?.username}
+              // src={currentUser?.profile_pic}
+              // alt={currentUser?.username}
             />
-          </Link>
+          {/* </Link> */}
           <form className={styles.form}>
             <TextArea
               className="chat"
@@ -56,8 +56,8 @@ export default function ChatCreate({ handleCreate }) {
           <p className={styles.counter}>{content.length}/280</p>
           <div className={styles.submit}>
             <Button
-              classList={!content ? 'btn disabled' : 'btn chat'}
-              onClick={() => handleCreate(chat)}
+              classList={!content ? 'btn disabled' : 'btn sm'}
+              // onClick={() => handleCreate(chat)}
               disabled={!content}
             >
               Chat
