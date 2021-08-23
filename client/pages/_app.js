5848d51db1501b14df130f4cd74e1b4c6e53fe31
Christian Bolus <christianjbolus@gmail.com>
2021-08-23 11:22:21 -0400

    Refactor _app.js to store currentUser state, handleLogin function and provide both through context

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
+import {useRouter} from 'next/router'
 import { HeadData } from '../components';
+import { loginUser, verifyUser } from '../services/auth';
+import { UserContext } from '../contexts/UserContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const router = useRouter()
+  useEffect(() => {
+    const handleVerify = async () => {
+      const res = await verifyUser();
+      setCurrentUser(res);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    if (userData.error) {
+      return userData.error;
+    } else {
+      setCurrentUser(userData);
+      router.push('/chats');
+    }
+  };
+
   return (
     <>
-      <HeadData />
-      <Component {...pageProps} />
+      <UserContext.Provider value={{ currentUser, handleLogin }}>
+        <HeadData />
+        <Component {...pageProps} />
+      </UserContext.Provider>
     </>
   );
 }
