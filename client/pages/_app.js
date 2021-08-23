4989ce280159032fd31bed119e3155b488b42ec0
Christian Bolus <christianjbolus@gmail.com>
2021-08-23 11:38:08 -0400

    Add handleRegister function to _app.js and provide through UserContext

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import {useRouter} from 'next/router'
 import { HeadData } from '../components';
-import { loginUser, verifyUser } from '../services/auth';
+import { loginUser, registerUser, verifyUser } from '../services/auth';
 import { UserContext } from '../contexts/UserContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const [currentUser, setCurrentUser] = useState(null);
   const router = useRouter()
+
   useEffect(() => {
     const handleVerify = async () => {
       const res = await verifyUser();
@@ -26,9 +27,19 @@ function MyApp({ Component, pageProps }) {
     }
   };
 
+  const handleRegister = async (formData) => {
+    const userData = await registerUser(formData);
+    if (userData.error) {
+      return userData.error
+    } else {
+      setCurrentUser(userData);
+      router.push('/');
+    }
+  };
+
   return (
     <>
-      <UserContext.Provider value={{ currentUser, handleLogin }}>
+      <UserContext.Provider value={{ currentUser, handleLogin, handleRegister }}>
         <HeadData />
         <Component {...pageProps} />
       </UserContext.Provider>
