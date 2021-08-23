296427760a1b14d977910f2fd8ef0499c2843fcf
Christian Bolus <christianjbolus@gmail.com>
2021-08-23 12:37:23 -0400

    Add getServerSideProps to [chatId].js page

--- client/pages/chats/[chatId]/index.js
+++ client/pages/chats/[chatId]/index.js
@@ -1,28 +1,25 @@
 import { useState, useEffect } from 'react';
-import { Link, useHistory, useParams } from 'react-router-dom';
+import { useRouter } from 'router/next';
+import Link from 'next/link';
 import { getOneChat } from '../services/chats';
 import { getAllReplies } from '../services/replies';
 import { Button, Chat, Engagement } from '../components';
 import { IoArrowBackOutline, IoChatbubbleOutline } from 'react-icons/all';
 
 import '../assets/css/screens/ChatDetail.css';
+//! Look at this file
+// import { icons } from '@react-icons/all-files/lib';
 
-export default function ChatDetail() {
-  const [chat, setChat] = useState(null);
+export default function ChatDetail({ chat }) {
   const [replies, setReplies] = useState(null);
-  const { id } = useParams();
-  const history = useHistory();
+  const router = useRouter();
+  const { chatId } = router.query;
 
   useEffect(() => {
-    const fetchChat = async () => {
-      const chatData = await getOneChat(id);
-      setChat(chatData);
-    };
     const fetchReplies = async () => {
-      const replyData = await getAllReplies(id);
+      const replyData = await getAllReplies(chatId);
       setReplies(replyData);
     };
-    fetchChat();
     fetchReplies();
     // eslint-disable-next-line
   }, []);
@@ -33,38 +30,38 @@ export default function ChatDetail() {
         <div className="chat-nav">
           <IoArrowBackOutline
             className="back-arrow"
-            onClick={() => history.push('/chats')}
+            onClick={() => router.push('/chats')}
           />
         </div>
         <div className="chat-detail-user">
-          <Link to={`/users/${chat?.user.username}`}>
+          <Link href={`/users/${chat.user.username}`}>
             <img
               className="chat-detail-user-profile-pic"
-              src={chat?.user.profile_pic}
-              alt={chat?.user.username}
+              src={chat.user.profile_pic}
+              alt={chat.user.username}
             />
           </Link>
           <div className="chat-detail-user-indetifiers">
             <p className="chat-detail-display-name">
-              {chat?.user.display_name}
+              {chat.user.display_name}
             </p>
-            <p className="chat-detail-username">@{chat?.user.username}</p>
+            <p className="chat-detail-username">@{chat.user.username}</p>
           </div>
         </div>
-        <p className="chat-detail-text">{chat?.content}</p>
+        <p className="chat-detail-text">{chat.content}</p>
         <Engagement
-          chatId={chat?.id}
-          userId={chat?.user_id}
-          replies={chat?.reply_count}
-          reposts={chat?.repost_count}
-          likes={chat?.like_count}
+          chatId={chat.id}
+          userId={chat.user_id}
+          replies={chat.reply_count}
+          reposts={chat.repost_count}
+          likes={chat.like_count}
           edit={true}
         />
         <div className="reply-button">
-          <Link to={`/chats/${chat?.id}/replies/new`}>
+          <Link to={`/chats/${chat.id}/replies/new`}>
             <Button className="btn btn-chat" text="Reply" />
           </Link>
-        </div> 
+        </div>
       </div>
       <div className="chat-list">
         {replies?.map(reply => (
@@ -72,15 +69,24 @@ export default function ChatDetail() {
             chat={reply}
             user={reply.user}
             key={reply.id}
-            url={`/chats/${id}/replies/${reply.id}`}
+            url={`/chats/${chatId}/replies/${reply.id}`}
           />
         ))}
       </div>
-      <Link to={`/chats/${chat?.id}/replies/new`}>
-        <Button className="btn btn-round fixed reply">
-          <IoChatbubbleOutline className="btn-icon" />
-        </Button>
-      </Link>
+      <Button
+        className="btn round fixed reply"
+        link={`/chats/${chat.id}/replies/new`}
+      >
+        <IoChatbubbleOutline className={icons.btn} />
+      </Button>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const chat = await getOneChat(id);
+  return {
+    props: { chat },
+  };
 }
