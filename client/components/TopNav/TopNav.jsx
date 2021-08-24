11c7536f1c6de2077c8c742a186a0638ce3a61d4
Christian Bolus <christianjbolus@gmail.com>
2021-08-24 11:12:51 -0400

    Add TopNav CSS module

--- client/components/TopNav/TopNav.jsx
+++ client/components/TopNav/TopNav.jsx
@@ -1,30 +1,31 @@
 import { useState, useContext } from 'react';
-import { UserContext } from '../contexts';
-import { BsChatDotsFill } from 'react-icons/bs';
-import '../assets/css/components/TopNav.css';
+import { UserContext } from '../../contexts/UserContext'
+import { BsChatDotsFill } from '@react-icons/all-files/bs/BsChatDotsFill'
 import PopoutNav from './PopoutNav';
+import styles from './TopNav.module.css'
+import icons from '../../styles/Icon.module.css'
 
 export default function TopNav({ location }) {
   const [show, setShow] = useState(false);
   const currentUser = useContext(UserContext);
   return (
     <>
-      <PopoutNav show={show} setShow={setShow} />
-      <div className="navbar-container">
-        <nav className="navbar">
-          <div className="navbar-pic">
+      {/* <PopoutNav show={show} setShow={setShow} /> */}
+      <div className={styles.container}>
+        <nav className={styles.navbar}>
+          <div className={styles.pic_container}>
             <img
-              className="navbar-profile-pic"
+              className={styles.profile_pic}
               src={currentUser?.profile_pic}
               onClick={() => setShow(true)}
               alt={currentUser?.username}
             />
           </div>
-          <div className="navbar-location-container">
-            <h2 className="navbar-location-text">{location}</h2>
+          <div className={styles.location}>
+            <h2 className={styles.location_text}>{location}</h2>
           </div>
-          <div className="navbar-logo-container">
-            <BsChatDotsFill className="navbar-logo" />
+          <div className={styles.logo}>
+            <BsChatDotsFill className={icons.navbar} />
           </div>
         </nav>
       </div>
