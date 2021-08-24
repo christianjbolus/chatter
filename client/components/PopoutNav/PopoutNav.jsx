9088a71d54694cb000397c9e03a0077c7256ff1b
Christian Bolus <christianjbolus@gmail.com>
2021-08-24 12:19:27 -0400

    Add PopoutNav CSS module

--- client/components/PopoutNav/PopoutNav.jsx
+++ client/components/PopoutNav/PopoutNav.jsx
@@ -1,60 +1,52 @@
-import { Link } from 'react-router-dom';
-import { Button, NavLinks } from './index';
-import { BiPlus, IoClose } from 'react-icons/all';
-import '../assets/css/components/PopoutNav.css';
-import UserMetrics from './UserMetrics';
+import Link from 'next/link';
+import { Button /*NavLinks, UserMetrics*/ } from './index';
+import { BiPlus } from '@react-icons/all-files/bi/BiPlus'
+import { IoClose } from '@react-icons/all-files/io5/IoClose'
+import styles from './PopoutNav.module.css';
+import icons from '../../styles/Icon.module.css';
 
 export default function PopoutNav({ show, setShow, currentUser }) {
-
   return (
     <div
-      className={show ? 'popout-nav-container show' : 'popout-nav-container'}
+      className={show ? `${styles.container} ${styles.show}` : styles.container}
     >
-      <div className="popout-nav">
-        <div className="popout-nav-header">
-          <IoClose
-            className="popout-nav-close"
-            onClick={() => setShow(false)}
-          />
-          <h2 className="popout-nav-header-text">Chatter</h2>
+      <div className={styles.nav}>
+        <div className={styles.header}>
+          <IoClose className={icons.close} onClick={() => setShow(false)} />
+          <h2 className={styles.header_text}>Chatter</h2>
         </div>
         {currentUser && (
-          <div className="popout-nav-user-profile">
-            <div className="popout-nav-user-container">
-              <Link
-                to={`/users/${currentUser?.username}`}
-                className="popout-nav-user"
-              >
-                <img
-                  className="popout-nav-profile-pic"
-                  src={currentUser?.profile_pic}
-                  alt={currentUser?.username}
-                />
-                <div className="popout-nav-user-identifiers">
-                  <p className="popout-nav-display-name">
-                    {currentUser?.display_name}
-                  </p>
-                  <p className="popout-nav-username">
-                    @{currentUser?.username}
-                  </p>
+          <div className={styles.profile}>
+            <div className={styles.user_container}>
+              <Link href={`/users/${currentUser?.username}`}>
+                <div className={styles.user}>
+                  <img
+                    className={styles.profile_pic}
+                    src={currentUser?.profile_pic}
+                    alt={currentUser?.username}
+                  />
+                  <div className="popout-nav-user-identifiers">
+                    <p className={styles.display_name}>
+                      {currentUser?.display_name}
+                    </p>
+                    <p className={styles.username}>@{currentUser?.username}</p>
+                  </div>
                 </div>
               </Link>
-              <Link to="/chats/new">
-                <Button className="btn btn-round-sm">
-                  <BiPlus className="btn-icon-sm" />
-                </Button>
-              </Link>
+              <Button className="btn round_sm" link="chats/compose">
+                <BiPlus className={icons.btn_sm} />
+              </Button>
             </div>
-            <UserMetrics user={currentUser} mode="dark" />
+            {/* <UserMetrics user={currentUser} mode="dark" /> */}
           </div>
         )}
         <div className="popout-nav-links">
-          <NavLinks
+          {/* <NavLinks
             size="link-sm"
             mode="link-dark"
             icon="link-icon-sm"
             collapsible={false}
-          />
+          /> */}
         </div>
       </div>
     </div>
