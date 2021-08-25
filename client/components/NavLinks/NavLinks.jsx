f302125b1b5a2b2c23e157f27e05a1d3db7aadc6
Christian Bolus <christianjbolus@gmail.com>
2021-08-25 10:01:25 -0400

    Refactor NavLinks component

--- client/components/NavLinks/NavLinks.jsx
+++ client/components/NavLinks/NavLinks.jsx
@@ -10,61 +10,62 @@ import icons from '../../styles/Icon.module.css';
 
 export default function NavLinks({ size, mode, icon, collapsible }) {
   const { currentUser, handleLogout } = useContext(UserContext);
+
+  if (!currentUser) {
+    return (
+      <>
+        <NavLink
+          text="Login"
+          size={size}
+          mode={mode}
+          href="/login"
+          className={`${collapsible ? 'collapse' : ''}`}
+        >
+          <AiOutlineLogin className={icons[icon]} />
+        </NavLink>
+        <NavLink
+          text="Sign Up"
+          size={size}
+          mode={mode}
+          href="/register"
+          className={`${collapsible ? 'collapse' : ''}`}
+        >
+          <IoPersonAddOutline className={icons[icon]} />
+        </NavLink>
+      </>
+    );
+  }
+  
   return (
     <>
-      {currentUser ? (
-        <>
-          <NavLink
-            text="My Profile"
-            size={size}
-            mode={mode}
-            href={`/users/${currentUser?.username}`}
-            className={`${collapsible ? 'collapse' : ''}`}
-          >
-            <IoPersonOutline className={icons[icon]} />
-          </NavLink>
-          <NavLink
-            text="Likes"
-            size={size}
-            mode={mode}
-            href={`/${currentUser?.username}/likes`}
-            className={`${collapsible ? 'collapse' : ''}`}
-          >
-            <BsHeart className={icons[icon]} />
-          </NavLink>
-          <NavLink
-            text="Logout"
-            size={size}
-            mode={mode}
-            href="#"
-            onClick={handleLogout}
-            className={`${collapsible ? 'collapse' : ''}`}
-          >
-            <FiPower className={icons[icon]} />
-          </NavLink>
-        </>
-      ) : (
-        <>
-          <NavLink
-            text="Login"
-            size={size}
-            mode={mode}
-            href="/login"
-            className={`${collapsible ? 'collapse' : ''}`}
-          >
-            <AiOutlineLogin className={icons[icon]} />
-          </NavLink>
-          <NavLink
-            text="Sign Up"
-            size={size}
-            mode={mode}
-            href="/register"
-            className={`${collapsible ? 'collapse' : ''}`}
-          >
-            <IoPersonAddOutline className={icons[icon]} />
-          </NavLink>
-        </>
-      )}
+      <NavLink
+        text="My Profile"
+        size={size}
+        mode={mode}
+        href={`/users/${currentUser?.username}`}
+        className={`${collapsible ? 'collapse' : ''}`}
+      >
+        <IoPersonOutline className={icons[icon]} />
+      </NavLink>
+      <NavLink
+        text="Likes"
+        size={size}
+        mode={mode}
+        href={`/${currentUser?.username}/likes`}
+        className={`${collapsible ? 'collapse' : ''}`}
+      >
+        <BsHeart className={icons[icon]} />
+      </NavLink>
+      <NavLink
+        text="Logout"
+        size={size}
+        mode={mode}
+        href="#"
+        onClick={handleLogout}
+        className={`${collapsible ? 'collapse' : ''}`}
+      >
+        <FiPower className={icons[icon]} />
+      </NavLink>
     </>
   );
 }
