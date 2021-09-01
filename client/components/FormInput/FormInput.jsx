51e755beab1a60cb37620ef5c68d71006386c674
Christian Bolus <christianjbolus@gmail.com>
2021-09-01 12:28:41 -0400

    Clean up unused code in FormInput component

--- client/components/FormInput/FormInput.jsx
+++ client/components/FormInput/FormInput.jsx
@@ -29,35 +29,3 @@ FormInput.defaultProps = {
 };
 
 export default FormInput
-// import styles from './FormInput.module.css';
-
-// export default function FormInput({
-//   type,
-//   label,
-//   name,
-//   value,
-//   ref,
-//   handleChange,
-//   errMessage,
-// }) {
-//   return (
-//     <div className={styles.form_field}>
-//       <label className={styles.label} htmlFor={name}>
-//         {label}
-//       </label>
-//       <input
-//         className={styles.input}
-//         id={name}
-//         type={type}
-//         name={name}
-//         value={value}
-//         onChange={handleChange}
-//       />
-//       <p className={styles.error}>{errMessage}</p>
-//     </div>
-//   );
-// }
-
-// FormInput.defaultProps = {
-//   type: 'text',
-// };
