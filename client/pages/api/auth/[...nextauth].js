037d58b719ed4f7e3ac09bf25f156eaabd9605cd
Christian Bolus <christianjbolus@gmail.com>
2021-09-01 09:48:52 -0400

    Add logic for login using next-auth providers

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,12 +1,24 @@
-import NextAuth from 'next-auth'
-import loginUser from '../../../services/auth'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import api from '../../../services/apiConfig';
 
 export default NextAuth({
+  session: {
+    jwt: true,
+  },
   providers: [
     Providers.Credentials({
       async authorize(credentials) {
-        const res = await loginUser(credentials)
-      }
-    })
-  ]
-})
+        const { username, password } = credentials;
+        try {
+          const user = await api.post('/auth/login', {
+            authentication: { username, password },
+          });
+          return user;
+        } catch (error) {
+          throw new Error('Invalid credentials');
+        }
+      },
+    }),
+  ],
+});
