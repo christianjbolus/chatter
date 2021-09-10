85521d4ca9896d5921cdc3a7d9264d8a37f1d63f
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-10 14:53:53 -0400

    Render DevModal on index route if guest user tries to create new Chat

--- client/pages/chats/index.js
+++ client/pages/chats/index.js
@@ -1,29 +1,49 @@
-import { useState, useEffect } from 'react'
-import Layout from '../../layout/Layout'
-import { Button, ChatList, TopNav } from '../../components';
+import { useState, useEffect, useContext } from 'react';
+import Layout from '../../layout/Layout';
+import { Button, ChatList, DevModal, TopNav } from '../../components';
 import { getAllChats } from '../../services/chats';
+import { AuthContext } from '../../contexts/AuthContext';
 import { BiPlus } from '@react-icons/all-files/bi/BiPlus';
 import icons from '../../styles/Icon.module.css';
 
 export default function Chats() {
-  const [allChats, setAllChats] = useState([])
+  const [allChats, setAllChats] = useState([]);
+  const [show, setShow] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchChats = async () => {
-      const res = await getAllChats()
-      setAllChats(res)
-    }
-    fetchChats()
-  },[])
+      const res = await getAllChats();
+      setAllChats(res);
+    };
+    fetchChats();
+  }, []);
 
   return (
-    <Layout>
+    <Layout setShow={setShow}>
+      <DevModal
+        setShow={setShow}
+        message="You must be logged in to use this feature"
+        className={show ? 'container active' : 'container'}
+      />
       <TopNav location="Chatter" />
-      <ChatList items={allChats} url="/chats/id"/>
-      <Button className="btn round fixed new" link="/chats/compose">
+      <ChatList items={allChats} url="/chats/id" />
+      {currentUser ? (
+        <Button className="btn round fixed new" link={'/chats/compose'}>
+          <BiPlus className={icons.btn} />
+        </Button>
+      ) : (
+        <Button className="btn round fixed new" onClick={() => setShow(true)}>
+          <BiPlus className={icons.btn} />
+        </Button>
+      )}
+      {/* <Button
+        className="btn round fixed new"
+        link={currentUser? "/chats/compose" : ''}
+        onClick={() => setShow(true)}
+      >
         <BiPlus className={icons.btn} />
-      </Button>
+      </Button> */}
     </Layout>
   );
 }
-
