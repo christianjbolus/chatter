533f60b79f4b05b6fb9204bb5a9f36552d09462d
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-10 09:46:12 -0400

    Add a default prop of '' for errMessage in FormInput component

--- client/components/FormInput/FormInput.jsx
+++ client/components/FormInput/FormInput.jsx
@@ -3,6 +3,7 @@ import styles from './FormInput.module.css';
 
 const FormInput = React.forwardRef((props, ref) => {
   const {type, label, name, value, handleChange, errMessage} = props
+  console.log(errMessage)
   return (
     <div className={styles.form_field}>
       <label className={styles.label} htmlFor={name}>
@@ -26,6 +27,7 @@ FormInput.displayName = 'FormInput'
 
 FormInput.defaultProps = {
   type: 'text',
+  errMessage: ''
 };
 
 export default FormInput
