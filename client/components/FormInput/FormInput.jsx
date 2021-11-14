0a9c4b3d9768a5e39576b1cf27ff9399ca21f94d
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-11-14 17:00:06 -0500

    Refactor FormInput component

--- client/components/FormInput/FormInput.jsx
+++ client/components/FormInput/FormInput.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import styles from './FormInput.module.css';
+import { IoWarning } from '@react-icons/all-files/io5/IoWarning'
+import styles from './FormInput.module.scss';
 
 const FormInput = React.forwardRef((props, ref) => {
-  const {type, label, name, value, placeholder, handleChange, errMessage} = props
-  console.log(errMessage)
+  
+  const { type, label, name, value, placeholder, handleChange, validation, errMessage } = props;
+
   return (
-    <div className={styles.form_field}>
-      <label className={styles.label} htmlFor={name}>
-        {label}
-      </label>
-      <input
-        className={styles.input}
-        id={name}
-        type={type}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        ref={ref}
-        onChange={handleChange}
-      />
-      <p className={styles.error}>{errMessage}</p>
+    <div className={styles.container}>
+      <div className={styles.form_field}>
+        <label className={styles.label} htmlFor={name}>
+          {label}
+        </label>
+        <input
+          className={errMessage ? `${styles.input} ${styles.err_input}` : styles.input}
+          id={name}
+          type={type}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          ref={ref}
+          onChange={handleChange}
+          onBlur={validation}
+        />
+        {errMessage && (
+          <p className={styles.error}>
+            <IoWarning className="error_icon" />
+            {errMessage}
+          </p>
+        )}
+      </div>
     </div>
   );
-})
+});
 
 FormInput.displayName = 'FormInput'
 
