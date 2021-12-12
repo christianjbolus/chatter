9fd4faa378ca178593b1c156a9856d183067e997
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-12 11:09:42 -0500

    Add update route for user profile edits

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   resources :chats do
     resources :replies
   end
+
   resources :users, only: :create
   get '/users/:username', to: 'users#show'
+  put '/users/:username', to: 'users#update'
   get '/users/:username/chats', to: 'users#user_chats'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
