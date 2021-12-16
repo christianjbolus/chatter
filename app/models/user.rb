a985ba7c2439f8df8dea8a724144ea905bf1d6f7
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-16 18:35:53 -0500

    Delete user's chats and replies when user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :chats
-  has_many :replies
+  has_many :chats, dependent: :destroy
+  has_many :replies, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
   validates :email, presence: true, uniqueness: true
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, on: :create
   validates :display_name, presence: true, length: { maximum: 25 }
   validates :bio, length: { maximum: 160 }
-  def chat_total
-    chats.count
-  end
+  # def chat_total
+  #   chats.count
+  # end
 end
