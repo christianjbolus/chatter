09e3eae85906bb2b2676463697eada863e7dca8e
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-30 09:13:30 -0500

    Remove old methods from services/auth.js

--- client/services/auth.js
+++ client/services/auth.js
@@ -1,16 +1,5 @@
 import api from './apiConfig';
 
-export const loginUser = async loginData => {
-  try {
-    const res = await api.post('/auth/login', { authentication: loginData });
-    localStorage.setItem('authToken', res.data.token);
-    api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
-    return res.data.user;
-  } catch (error) {
-    return {error: error.response.data.errors}
-  }
-};
-
 export const registerUser = async registerData => {
   try {
     const res = await api.post('/users/', { user: registerData });
@@ -19,17 +8,6 @@ export const registerUser = async registerData => {
     return {error: error.response.data}
   }
 };
-
-// export const registerUser = async registerData => {
-//   try {
-//     const res = await api.post('/users/', { user: registerData });
-//     localStorage.setItem('authToken', res.data.token);
-//     api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
-//     return res.data.user;
-//   } catch (error) {
-//     return {error: error.response.data}
-//   }
-// };
 
 export const verifyUser = async () => {
   try {
