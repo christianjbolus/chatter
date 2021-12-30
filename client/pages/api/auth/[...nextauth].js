5f9f4e9640342f81f1a0b849f2e7b1013084d965
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-30 08:49:49 -0500

    Add configuration for next-auth credentials provider

---
+++ client/pages/api/auth/[...nextauth].js
@@ -0,0 +1,57 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import api from '../../../services/apiConfig';
+
+export default NextAuth({
+  session: {
+    strategy: 'jwt',
+    maxAge: 24 * 30 * 60 * 60,
+  },
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        console.log('authorize', credentials);
+        const { username, password } = credentials;
+        try {
+          const {
+            data: { userData, accessToken },
+          } = await api.post('/auth/login', {
+            authentication: { username, password },
+          });
+          return { userData, accessToken };
+        } catch (error) {
+          throw new Error('Invalid credentials');
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    redirect: async ({url, baseUrl}) => {
+      return baseUrl;
+    },
+
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.currentUser = user.userData;
+        token.accessToken = user.accessToken;
+      } else {
+        token.accessToken = await refreshToken(token)
+      }
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+      session.currentUser = token.currentUser;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+
+async function refreshToken(token) {
+  const res = await api.post('/auth/refresh', { authentication: { id: token.currentUser.id } });
+  return res.data.accessToken;
+}
