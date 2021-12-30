9e5df1dab7c6d823257d81a2d4a9b968d218fdec
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-30 09:16:12 -0500

    Refactor apiConfig to set header with accessToken from session

--- client/services/apiConfig.js
+++ client/services/apiConfig.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
+import { getSession } from 'next-auth/react';
 
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
       ? 'https://chatter-app-server.herokuapp.com/'
       : 'http://localhost:3000',
+});
+
+api.interceptors.request.use(async config => {
+  const session = await getSession();
+  config.headers.authorization = `Bearer ${session?.accessToken}`;
+  return config;
 });
 
 export default api;
