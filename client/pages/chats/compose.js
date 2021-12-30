91e7b783ab7bebc46c806dd17e89e4d5d7a08829
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-12-30 09:18:11 -0500

    Add server side page guard on compose page

--- client/pages/chats/compose.js
+++ client/pages/chats/compose.js
@@ -70,3 +70,19 @@ export default function ChatCreate() {
     </Layout>
   );
 }
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  console.log(session);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: session,
+  };
+}
