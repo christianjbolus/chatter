28eb0c183ded42cb33c47498f89e7bd3f87a96e1
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2022-01-01 11:13:11 -0500

    Add client side form validations on register page

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,34 +1,51 @@
 import { useState, useEffect, useContext, useRef } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import Link from 'next/link';
 import { Button, FormInput, Password } from '../components';
+import validations from '../utils/validation';
 import { formatErrors } from '../utils/helpers';
 import styles from '../styles/AuthForm.module.scss';
-import { AuthContext } from '../contexts/AuthContext';
 
 export default function Register() {
   const [formData, setFormData] = useState({
     email: '',
     username: '',
     password: '',
-    display_name: '',
-    profile_pic: '',
-    bio: '',
   });
-
   const [formErrors, setFormErrors] = useState({});
+  const [isCompleted, setIsCompleted] = useState(false);
 
   const { register } = useContext(AuthContext);
   const inputRef = useRef();
 
   const { email, username, password } = formData;
 
+  // Auto focus first input field
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
+  // Activate button when all fields are filled and no errors
+  useEffect(() => {
+    if (validations.validateAll(formData) && !validations.hasEmptyField(formData)) {
+      setIsCompleted(true);
+    } else {
+      setIsCompleted(false);
+    }
+  }, [formData, formErrors]);
+
   const handleChange = e => {
     const { name, value } = e.target;
     setFormData(prevState => ({ ...prevState, [name]: value }));
+    setFormErrors(prevState => ({ ...prevState, [name]: '' }));
+  };
+
+  // Dynamically call appropriate validation function based on input name
+  // and set error message
+  const handleValidation = e => {
+    const { name, value } = e.target;
+    let message = validations[name](value);
+    setFormErrors(prevState => ({ ...prevState, [name]: message }));
   };
 
   const handleRegister = async e => {
@@ -52,6 +69,7 @@ export default function Register() {
               value={email}
               ref={inputRef}
               handleChange={handleChange}
+              validation={handleValidation}
               errMessage={formErrors.email}
             />
             <FormInput
@@ -67,10 +85,16 @@ export default function Register() {
               data="validate"
               value={password}
               handleChange={handleChange}
+              validation={handleValidation}
               setFormErrors={setFormErrors}
             />
             <div className={styles.submit}>
-              <Button className="btn auth">Create account</Button>
+              <Button
+                className={isCompleted ? 'btn auth' : 'btn auth disabled'}
+                disabled={!isCompleted}
+              >
+                Create account
+              </Button>
             </div>
           </form>
           <p className={styles.redirect}>
