4014f1d69421f5a9495f357149eef7ac17a8db71
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2022-01-12 10:58:33 -0500

    Remove formErrors from dependency array in register pages's second useEffect

--- client/pages/register.js
+++ client/pages/register.js
@@ -35,7 +35,7 @@ export default function Register() {
     } else {
       setIsCompleted(false);
     }
-  }, [formData, formErrors]);
+  }, [formData]);
 
   const handleChange = e => {
     const { name, value } = e.target;
