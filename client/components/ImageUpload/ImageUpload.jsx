988b134173e789976f92fb3ecd3df5b7e321ccfa
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2022-05-07 09:16:36 -0400

    Add initial markup

--- client/components/ImageUpload/ImageUpload.jsx
+++ client/components/ImageUpload/ImageUpload.jsx
@@ -2,6 +2,10 @@ import styles from './ImageUpload.module.scss';
 
 export default function ImageUpload() {
   return (
-    <div>ImageUpload</div>
-  )
+    <div class={styles.container}>
+      <label class={styles.label} for="img_upload">Choose file</label>
+      <input class={styles.input} type="file" accept="image/jpeg, image/png, image/webp" id="img_upload" />
+      <span class={styles.filename_display} id="img_upload">None</span>
+    </div>
+  );
 }
